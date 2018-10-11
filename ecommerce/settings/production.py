8544df2eb1fe351968a55e7bf7cd68f13e7bd491
Sojol Mia <Sojol Mia>
2018-10-11 10:14:04 +0600

    fix secrect key

--- ecommerce/settings/production.py
+++ ecommerce/settings/production.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '#=nz!4!@m(@6frolhw+b8*8y6mvnndg^^%z=nz3lz*yem-t&xu'
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', '#=nz!4!@m(@6frolhw+b8*8y6mvnndg^^%z=nz3lz*yem-t&xu')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
